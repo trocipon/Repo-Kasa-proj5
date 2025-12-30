9ea8edaad60dca40b8d4081e30da8795755eaff2
trocipon <thibaud.rocipon@protonmail.com>
2025-12-30 15:10:31 +0100

    Routing with paths & imports

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,24 @@
-import { useState } from "react"; // quelle fonctionnalité ? (template Vite)
-import Header from './Header';
-import Main from './Main';
-import Footer from './Footer';
-import "../styles/App.scss";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./Header";
+import Home from "../pages/Home";
+import Rental from "../pages/Rental";
+import About from "../pages/About";
+import NotFound from "../pages/Notfound";
+import Footer from "./footer";
 
 export default function App() {
   return (
-    <>
-      <Header />
-      <Main>
-        {/* Composants à adapter pour chaque page ? */}
-      </Main>
+    <Router>
+      <div className="mainContainer">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/fiche-logement/:id" element={<Rental />} />
+          <Route path="/about" element={<About />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
       <Footer />
-    </>
+    </Router>
   );
 }
