bd65f2942063f90c5328a3e841aa4f98bdd1cdda
trocipon <thibaud.rocipon@protonmail.com>
2025-12-30 15:02:13 +0100

    install FA (depencies)

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./styles/index.scss";
 import App from "./components/App.jsx";
+import "./styles/main.scss";
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+/* import all the icons in Free Solid, Free Regular, and Brands styles */
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { far } from "@fortawesome/free-regular-svg-icons";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+
+library.add(fas, far, fab);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
