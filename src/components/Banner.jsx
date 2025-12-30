43e2e17552e0f85a5f43d63cc8625f1fdb83bef8
trocipon <thibaud.rocipon@protonmail.com>
2025-12-30 16:25:31 +0100

    fix banner rendering in home & about page

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,7 +1,12 @@
 export default function Banner({ text, picture, className = "" }) {
   return (
     <div className={`bannerContainer ${className}`}>
-      <img src={picture} className="bannerPicture" />
+      <img
+        src={picture}
+        className="bannerPicture"
+        loading="eager"
+        fetchPriority="high"
+      />
       {text && <h1 className="banner-overlay">{text}</h1>}
     </div>
   );
