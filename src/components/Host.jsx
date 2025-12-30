08f924fe1987dad60874254fab338694730f195c
trocipon <thibaud.rocipon@protonmail.com>
2025-12-30 18:31:28 +0100

    host component : split name and surname

--- src/components/Host.jsx
+++ src/components/Host.jsx
@@ -1,8 +1,15 @@
-
 export default function Host({ host }) {
+  const parts = host.name.split(" ");
+  const last = parts.pop();
+  const first = parts.join(" ");
+
   return (
     <div className="host-container">
-      <p className="host-name">{host.name}</p>
+      <p className="host-name">
+        {first}
+        <br />
+        {last}
+      </p>
       <img className="host-picture" src={host.picture} />
     </div>
   );
