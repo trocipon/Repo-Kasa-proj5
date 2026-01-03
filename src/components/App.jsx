6c16b15a2a667378c9374edc7252a0108064e581
trocipon <thibaud.rocipon@protonmail.com>
2026-01-03 01:04:12 +0100

    remove react.lazy & suspense in App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,26 +1,22 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./Header";
 import Home from "../pages/Home";
+import Rental from "../pages/Rental";
+import About from "../pages/About";
+import NotFound from "../pages/Notfound";
 import Footer from "./footer";
-import React, { Suspense } from "react";
-
-const Rental = React.lazy(() => import("../pages/Rental"));
-const About = React.lazy(() => import("../pages/About"));
-const NotFound = React.lazy(() => import("../pages/Notfound"));
 
 export default function App() {
   return (
     <Router>
       <div className="mainContainer">
         <Header />
-        <Suspense fallback={<div>Chargement...</div>}>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/fiche-logement/:id" element={<Rental />} />
-            <Route path="/about" element={<About />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </Suspense>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/fiche-logement/:id" element={<Rental />} />
+          <Route path="/about" element={<About />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
       </div>
       <Footer />
     </Router>
